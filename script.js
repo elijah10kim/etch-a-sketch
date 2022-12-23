b33d3cce12fb1c2beb025a9d969775d7dc1e1e4f
Elijah <elijah10kim@gmail.com>
2022-12-23 18:31:46 -0500

    fixed createGrid function to be 16x16

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const DEFAULT_SIZE = 16;
 const grid = document.getElementById('grid');
 
 function createGrid(n) {
-    for (let i=0; i < n; i++) {
+    grid.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+    for (let i=0; i < n*n; i++) {
         const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
         grid.appendChild(gridSquare);
     }
 }

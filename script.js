1111157be967ac89117cbb4153f0c16bf5e96b85
Elijah <elijah10kim@gmail.com>
2022-12-27 15:51:52 -0500

    added clearGrid function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const DEFAULT_SIZE = 16;
 
 const grid = document.getElementById('grid');
+const clearGridBtn = document.getElementById('clear-grid');
+clearGridBtn.addEventListener('click', clearGrid);
 
 function createGrid(n) {
     grid.style.gridTemplateRows = `repeat(${n}, 1fr)`;
@@ -15,6 +17,13 @@ function createGrid(n) {
 
 function fillSquare(e) {
     e.target.style.background = 'black';
+}
+
+function clearGrid() {
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach((gridSquare) => {
+        gridSquare.style.background = 'white';
+    })
 }
 
 window.onload = () => {

6551532e1d4e05765722b09e9e918675ca106ffb
Elijah <elijah10kim@gmail.com>
2022-12-27 16:22:27 -0500

    added eraser functionality to eraser button

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const DEFAULT_SIZE = 16;
 
 const grid = document.getElementById('grid');
+
 const clearGridBtn = document.getElementById('clear-grid');
 clearGridBtn.addEventListener('click', clearGrid);
+
+const resizeGridBtn = document.getElementById('grid-size');
+resizeGridBtn.addEventListener('click', resizeGrid);
+
+const eraserBtn = document.getElementById('eraser');
+eraserBtn.addEventListener('click', () => {eraserMode = true})
+
+let eraserMode = false;
 
 function createGrid(n) {
     grid.style.gridTemplateRows = `repeat(${n}, 1fr)`;
@@ -10,13 +19,26 @@ function createGrid(n) {
     for (let i=0; i < n*n; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
-        gridSquare.addEventListener('mouseover', fillSquare)
+        gridSquare.addEventListener('mouseover', fillSquare);
+        gridSquare.addEventListener('mouseover', eraseSquare);
         grid.appendChild(gridSquare);
     }
 }
 
 function fillSquare(e) {
-    e.target.style.background = 'black';
+    if (!eraserMode) {
+        e.target.style.background = 'black';
+    }
+}
+
+function eraseSquare(e) {
+    if (eraserMode) {
+        e.target.style.background = 'white';
+    }
+}
+
+function resizeGrid() {
+    let newSize = prompt("How many grid squares per side would you like? Enter a number between 2 and 100.");
 }
 
 function clearGrid() {

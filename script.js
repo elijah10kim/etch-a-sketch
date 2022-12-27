01832c8162cc807920655a0d2484bc0ac946ef1e
Elijah <elijah10kim@gmail.com>
2022-12-27 16:57:34 -0500

    fixed pen mode toggle and added active mode style

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const DEFAULT_SIZE = 16;
 const grid = document.getElementById('grid');
 
 const singleColourBtn = document.getElementById('single-colour');
-singleColourBtn.addEventListener('click', () => {eraserMode = false})
+singleColourBtn.addEventListener('click', setActivePenMode)
 
 const clearGridBtn = document.getElementById('clear-grid');
 clearGridBtn.addEventListener('click', clearGrid);
@@ -12,7 +12,7 @@ const resizeGridBtn = document.getElementById('grid-size');
 resizeGridBtn.addEventListener('click', resizeGrid);
 
 const eraserBtn = document.getElementById('eraser');
-eraserBtn.addEventListener('click', () => {eraserMode = true})
+eraserBtn.addEventListener('click', setActivePenMode);
 
 let eraserMode = false;
 
@@ -37,6 +37,23 @@ function fillSquare(e) {
 function eraseSquare(e) {
     if (eraserMode) {
         e.target.style.background = 'white';
+    }
+}
+
+//add this function as a check to each of click listener functions for
+//single colour button, theme button and eraser button.
+//add it after setting eraserMode value on click
+function setActivePenMode(e) {
+    if (!eraserMode && e.target.id == 'eraser') {
+        //toggle css class for (active-button) based on boolean value
+        singleColourBtn.classList.toggle('active-pen-mode');
+        eraserBtn.classList.toggle('active-pen-mode');
+        eraserMode = true;
+    }
+    if (eraserMode && e.target.id == 'single-colour') {
+        singleColourBtn.classList.toggle('active-pen-mode');
+        eraserBtn.classList.toggle('active-pen-mode');
+        eraserMode = false;
     }
 }
 
